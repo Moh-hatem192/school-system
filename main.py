a047078d3276e5eb98248d2800212ad19184fdfa
Moh-hatem192 <mohkawasmi2006@gmail.com>
2023-12-19 01:24:18 +0300

    teacher class added

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ class Student:
     def __str__(self):
         return f'{self.name} is {self.age} years old and in {self.grade} grade'
 
+    def __repr__(self):
+        return f'Student(Name: {self.name} is in {self.grade} grade )'
+    
     @classmethod
     def create_instances(cls, file):
         with open(file) as f:
@@ -26,3 +29,41 @@ class Student:
                     student.get('name'),
                     student.get('name'),
                 ) 
+                
+class Teacher:
+    All_Teachers = []
+    def __init__(self, name, age, nationality, email, hours_worked, hourly_rate):
+        self.name = name
+        self.age = age
+        self.__email = email
+        self.nationality = nationality
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+        self.nationality = nationality   
+        Teacher.All_Teachers.append(self)
+
+    def __str__(self):
+        return f'{self.name} is {self.age} years old for conatact: {self.__email}'
+
+    def __repr__(self):
+        return f'Teacher(Name: {self.name} is  {self.age} years old )'
+    
+    @property
+    def email(self):
+        return self.__email
+    
+    @classmethod
+    def create_instances(cls, file):
+        with open(file) as f:
+            teachers = csv.DictReader(f)
+            for teacher in teachers:
+                Student(
+                    teacher.get('name'),
+                    teacher.get('name'),
+                    teacher.get('name'),
+                    teacher.get('name'),
+                    teacher.get('name'),
+                    teacher.get('name')
+                ) 
+                        
+osama = Teacher('ali',13,45,'asw','ahh',12)
